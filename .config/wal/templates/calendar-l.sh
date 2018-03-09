604e7112dc806988b61ef99a75b74551f5240c90
mananapr <manny011@live.com>
2018-03-09 20:32:27 +0530

    Added pywal templates for bar popups

---
+++ .config/wal/templates/calendar-l.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+source ~/.cache/wal/colors.sh
+
+# vars
+cil="/tmp/calendar.png"
+bg="/home/manan/Scripts/popup/img/bg.png"
+width=$(xdotool "getdisplaygeometry" | awk '{{print $1;}}')
+height=$(xdotool "getdisplaygeometry" | awk '{{print $2;}}')
+ypos=$(expr "$height" - "242")
+xpos=$(expr "$width")
+
+# convert the output to png
+convert -background "rgba(0,0,0,0)" \
+		-fill "{color0}" \
+		-font "Monaco" \
+		-antialias \
+		-pointsize 12 \
+		label:"$(date "+%d %B %Y\n" && cal | tail -n7)" \
+		"$cil"
+
+# display it
+/home/manan/Scripts/popup/popup.sh "" "$(expr "$width" - "1340")" -p "70" &
+sleep ".05s"
+n30f -x "$(expr "$width" - "1310")" -y "$(expr "$ypos" + "-400")" -c "killall n30f" "$cil"
+
+# delete it
+sleep ".2s"
+rm "$cil"
---
+++ .config/wal/templates/popup-l.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# usage:
+# popup [file] [placement] -p [pointer location]
+
+# fetch the colors
+source "/home/manan/Scripts/colors.sh"
+
+
+# vars
+background="/home/manan/Scripts/popup/img/bg2.png"
+pointer_main="/home/manan/Scripts/popup/img/pointer.png"
+pointer="/home/manan/Scripts/popup/img/pointer_mod.png"
+y="80"
+
+# make bg.png from pywal color0
+convert -size 200x200 xc:"{color3}" "$background"
+convert -border 5x5 -bordercolor "{color0}" "$background" "$background"
+# replace the color in pointer.png to pywal color7
+convert "$pointer_main" -fuzz 50% -fill "{color0}" -opaque "#f2f2f2" "$pointer"
+
+# exec
+n30f -x "$2" -y "$(expr $y + 6)" -c "killall n30f" "$background" &
+sleep ".05s"
+n30f -x "$(expr "$2" + 5)" -y "$(expr $y + 11)" -c "killall n30f" "$1" &
+
+rm "$background"
+
+# pointer
+if [ "$3" == "-p" ] ; then
+	n30f -x "$(expr "$2" + "$4")" -y "$y" -c "killall n30f" "$pointer"
+    rm "$pointer"
+else
+	exit
+fi
